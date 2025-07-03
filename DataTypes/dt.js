40ad59f7a74abb444cd6d0d7865f8bc6b56e34eb
ShubhGupta75 <136063107+ShubhGupta75@users.noreply.github.com>
2025-07-03 20:40:38 +0000

    datatypes

---
+++ DataTypes/dt.js
@@ -0,0 +1,22 @@
+"use strict"; /* treat all JS ocde as newer version,
+ however it is automatically settled in 
+ newer version JS. */
+
+// alert(3+3) // we are using nodejs not browser
+
+// PRIMITIVE DATATYPES
+
+let name = "Shubh"  // string => "," 
+let age = 19  // number => 2^53 values, for bigger values use 'bigint'.
+let isLoggedIn = true // boolean => true/false
+// null => standalone value, basically empty
+// undefined => not assigned variables
+// symbol => used for uniqueness
+
+// NON PRIMITIVE DATATYPES
+
+// object
+
+console.log(typeof null); // type is object 
+console.log(typeof undefined); // type is undefined
+
