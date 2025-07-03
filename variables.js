380ebef7299f48ef86e18c58a2fde5f6c6d8c019
ShubhGupta75 <136063107+ShubhGupta75@users.noreply.github.com>
2025-07-03 20:38:46 +0000

    Variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+const accountId = 128393
+let emailId = "guptashubh780@gmail.com"
+var password = "12345"
+myName = "Shubh"
+
+// modification is not allowed in const
+
+console.log(emailId);
+console.table([accountId,emailId,password,myName]);
+
+/* 
+Prefer not to use var, because of issue in block scope and functional scope 
+*/
+
+
